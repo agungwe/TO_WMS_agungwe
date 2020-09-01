81571207c355b82a4840a7c987e1465d8776f369
agungwe <agungw@ecampus.ut.ac.id>
2020-09-01 22:49:46 +0700

    Update Push TO

--- app/controllers/request.controller.js
+++ app/controllers/request.controller.js
@@ -1,63 +1,138 @@
-const db = require("../models/index");
+var jwt = require('jsonwebtoken')
+var bcrypt = require('bcrypt')
+const db = require('../models/index')
+const Product = db.products
+const Request = db.requests
+const User = db.users
+exports.createProduct = async function (req,res){
+    let date_ob = new Date();
+    // adjust 0 before single digit date
+    let date = ("0" + date_ob.getDate()).slice(-2)
+    // current month
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2)
+    // current year
+    let year = date_ob.getFullYear()
+    // current hours
+    let hours = date_ob.getHours()
+    // current minutes
+    let minutes = date_ob.getMinutes()
+    // current seconds
+    let seconds = date_ob.getSeconds()
+    // date & time in YYYY-MM-DD HH:MM:SS format
+    let full_date = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds
 
-const Request = db.requests;
+    const dataIn = {
+        productId:req.body.product_id,
+        date:full_date,
+        total:req.body.total
+    }
 
-exports.createRequest = function (req, res) {
+    await Request.create(dataIn)
+             .then(async (data)=>{
+                Product.findByPk(req.body.product_id,{attributes:['stock']})
+                       .then(async (products)=>{
+                        let prod = JSON.parse(JSON.stringify(products, null, 4))
+                        let stock = prod.stock
+                        let total_stock = stock+req.body.total
 
-    //Validate request
-    if(!req.body.date || !req.body.total) {
-        res.status(400).send(
-            {
-                message: "can not be empty"
+                   Product.update({stock:total_stock},{where:{id:req.body.product_id}})
+                          .then(async (data)=>{
+                            let update_prod = JSON.parse(JSON.stringify(data, null, 4))
+                            console.log(update_prod)
+                            res.send({
+                                product_id:req.body.product_id,
+                                date:full_date,
+                                total_in:req.body.total,
+                                total_stock:total_stock
+                            })
+                          })
+                })           
+             })
+}
+
+exports.getProductById = async function (req,res){
+    await Request.findByPk(req.params.req_id,{
+                        attributes:['id','date','total'],
+                        include:[{
+                            model:Product,
+                            as:'product',
+                            attributes:['name','stock','price'],
+                            include:[{
+                                model:User,
+                                as:'suplier',
+                                attributes:['id','full_name','username','email','phone_number']
+                            }]
+                        }]
+                    })
+           .then(async (requests)=>{
+            let request_in = JSON.parse(JSON.stringify(requests, null, 4))
+            const prod ={
+                name: prod.product.name,
+                stock: prod.product.stock,
+                price: prod.product.price,
+                suplier:prod.product.suplier
             }
-        );
-        return;
-    }
+            const data = {
+                id: prod.id,
+                date: Date(prod.date),
+                total: prod.total,
+                product:products
+            }
+               res.send({
+                message:"succes get data",
+                status: "success",
+                data:data
+               })
+            
+           })
+}
 
-    const request = {
-        productId: req.body.productId,
-        date: req.body.date,
-        total: req.body.total,
-        userId: req.body.userId,
-    }
+exports.getProductAll = async function (req,res){
 
-    Request.create(request) // Insert into users
-        .then((data) => {
-            res.send(data);
-        }).catch((err) => {
-            res.status(500).send({
-                message: err.message ||
-                    "Some error occured while creating the Request"
+    const pagination = parseInt(req.params.pagination)
+    const limit      = parseInt(req.params.limit)
+    const offset     =  limit-(limit/pagination)
+
+   await Request.findAll({attributes:['id','date','total'],
+                        include:[{
+                            model:Product,
+                            as:'product',
+                            attributes:['name','stock','price'],
+                            include:[{
+                                model:User,
+                                as:'suplier',
+                                attributes:['id','full_name','username','email','phone_number']
+                            }]
+                        }],
+                    offset:offset,limit:limit})
+            .then(async (products)=>{
+
+                let data = []
+                for (const key in products) {
+                    if (products.hasOwnProperty(key)) {
+                        const prod = products[key];
+                        const product_detail ={
+                            name: prod.product.name,
+                            stock: prod.product.stock,
+                            price: prod.product.price,
+                            suplier:prod.product.suplier
+                        }
+                       let detail = {
+                                id: prod.id,
+                                date: Date(prod.date),
+                                total: prod.total,
+                                product:product_detail
+                        }
+                        data.push(detail)
+                    }
+                }
+                res.send({
+                    message:"succes get data",
+                    status: "success",
+                    data:data,
+                    totalItems:data.length,
+                    totalpages:parseInt(data.length/offset)?  parseInt(data.length/offset) : 1,
+                    currentpage:pagination
+                   })
             })
-        });
-    
-};
-
-
-
-//Query cari data requestId
-exports.findProductByRequestId = function (req, res) {
-    console.log(req.params.requestId);
-    
-    Product.findByPk(
-        
-        req.params.requestId, 
-        { 
-            attributes: ['id', 'date','total'],
-            include: [{
-                model: Product,
-                attributes: ['name','stock','price'],
-                include: [{
-                    model: User,
-                    attributes: ['id','full_name','username','email','phone_number']
-                }]
-            }]
-        }
-    ).then((data) => {  
-            res.send(data);
-        //response.ok(data,res)
-        })
-        .catch((err) => {
-            console.log(">> Error while finding tutorial: ", err);
-    });
-};
+}
--- app/routes/request.routes.js
+++ app/routes/request.routes.js
@@ -1,12 +1,14 @@
 module.exports = app => {
     const requests = require("../controllers/request.controller");
+    const auth = require("../middleware/auth")
+    let router = require("express").Router()
 
-    let router = require("express").Router();
+    //Simpan Baru
+    router.post("/", requests.createProduct)
+    //Cari Per Id
+    router.get("/:req_id", requests.getProductById)
+    //Cari Semua Data per Page
+    router.get("/all/:limit/:pagination", requests.getProductAll)
 
-    //create a new Order
-    router.post("/", requests.createRequest);
-
-    router.get("/request/:requestId", requests.findProductByRequestId );
-
-    app.use("/api/v1/request", router);
+    app.use("/api/v1/request/",auth.isAuth,router)
 }
